9f9fd27b48d86a25808cf5a203e7444755b2c8e5
Angelo Elias Dal Zotto <angelodalzotto97@gmail.com>
2023-06-09 15:48:08 -0300

    Enable radeonmem warning and critical

--- waybar/custom_modules/radeonmem.py
+++ waybar/custom_modules/radeonmem.py
@@ -3,10 +3,21 @@ import sys, json
 
 def radeonmem():
 	out = {}
+	used  = int(open("/sys/class/drm/card0/device/mem_info_vram_used").readline()[:-1])
+	total = int(open("/sys/class/drm/card0/device/mem_info_vram_total").readline()[:-1])
 	out["text"] = "{}G/{}G".format(
-		round(int(open("/sys/class/drm/card0/device/mem_info_vram_used").readline()[:-1])/1024/1024/1024, 1),
-		round(int(open("/sys/class/drm/card0/device/mem_info_vram_total").readline()[:-1])/1024/1024/1024, 1)
+		round(used/1024/1024/1024, 1),
+		round(total/1024/1024/1024, 1)
 	)
+
+	percentage = used/total
+	if percentage < 0.75:
+		out["class"] = "normal"
+	elif used/total < 0.9:
+		out["class"] = "warning"
+	else:
+		out["class"] = "critical"
+
 	sys.stdout.write(json.dumps(out) + '\n')
 	sys.stdout.flush()	
 
